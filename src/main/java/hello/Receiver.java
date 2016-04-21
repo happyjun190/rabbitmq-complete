9335cd53e07d5f9c21fa20459caef4bece77632f
shenjun-company <shenjun@126.com>
2016-04-21 18:40:01 +0800

    rabbitmq sample code

---
+++ src/main/java/hello/Receiver.java
@@ -0,0 +1,30 @@
+package hello;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
+
+import com.rabbitmq.client.Channel;
+
+public class Receiver implements ChannelAwareMessageListener {
+
+	private CountDownLatch latch = new CountDownLatch(1);
+
+	public void receiveMessage(String message) {
+		System.out.println("Received <" + message + ">");
+		latch.countDown();
+	}
+
+	public CountDownLatch getLatch() {
+		return latch;
+	}
+
+	
+	@Override
+	public void onMessage(Message message, Channel channel) throws Exception {
+		System.out.println("Received <" + new String(message.getBody()) + ">");
+		channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+	}
+
+}
